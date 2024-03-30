39175c4bebad2bd50c2fe2a0e9c7523e3e4ecd8c
OnkarDeshmukh67 <120507028+OnkarDeshmukh67@users.noreply.github.com>
2024-03-31 02:52:49 +0530

    add json file

---
+++ Frontend/src/Store/Locations.js
@@ -0,0 +1,152 @@
+export default Locations = [
+  {
+      "Location": "Taj Mahal",
+      "Latitude": 27.1751,
+      "Longitude": 78.0421,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Red Fort",
+      "Latitude": 28.6562,
+      "Longitude": 77.241,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Qutub Minar",
+      "Latitude": 28.5245,
+      "Longitude": 77.1855,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Gateway of India",
+      "Latitude": 18.9217,
+      "Longitude": 72.8347,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "India Gate",
+      "Latitude": 28.6129,
+      "Longitude": 77.2295,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Hawa Mahal",
+      "Latitude": 26.9239,
+      "Longitude": 75.8267,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Amer Fort",
+      "Latitude": 26.9855,
+      "Longitude": 75.8513,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Golden Temple",
+      "Latitude": 31.62,
+      "Longitude": 74.8765,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Ajanta Caves",
+      "Latitude": 20.5513,
+      "Longitude": 75.7037,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Ellora Caves",
+      "Latitude": 20.0274,
+      "Longitude": 75.178,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Meenakshi Temple",
+      "Latitude": 9.9195,
+      "Longitude": 78.119,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Victoria Memorial",
+      "Latitude": 22.545,
+      "Longitude": 88.3426,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Jantar Mantar",
+      "Latitude": 26.9339,
+      "Longitude": 75.8278,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Charminar",
+      "Latitude": 17.3616,
+      "Longitude": 78.4747,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Mysore Palace",
+      "Latitude": 12.3052,
+      "Longitude": 76.6552,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Udaipur City Palace",
+      "Latitude": 24.576,
+      "Longitude": 73.6836,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Amer Palace",
+      "Latitude": 26.9855,
+      "Longitude": 75.8513,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Sabarmati Ashram",
+      "Latitude": 23.0275,
+      "Longitude": 72.5745,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Lotus Temple",
+      "Latitude": 28.5535,
+      "Longitude": 77.2588,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Chhatrapati Shivaji Terminus",
+      "Latitude": 18.9401,
+      "Longitude": 72.8352,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Jaisalmer Fort",
+      "Latitude": 26.9124,
+      "Longitude": 70.9127,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Rashtrapati Bhavan",
+      "Latitude": 28.6142,
+      "Longitude": 77.1996,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Hampi",
+      "Latitude": 15.335,
+      "Longitude": 76.4621,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Jaisalmer",
+      "Latitude": 26.9124,
+      "Longitude": 70.9127,
+      "Street View Link": "Street View"
+  },
+  {
+      "Location": "Khajuraho",
+      "Latitude": 24.8318,
+      "Longitude": 79.9192,
+      "Street View Link": "Street View"
+  }
+]
