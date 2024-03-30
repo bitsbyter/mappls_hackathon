372e2ea7b4c35a3e171f7c9e330a7a4d215c7ee8
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-03-31 00:03:46 +0530

    Map-component ReadyToUse

--- Frontend/src/Components/Options/Options.jsx
+++ Frontend/src/Components/Options/Options.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import Map from '../Map'
+import { useSelector } from 'react-redux'
+import store from '../../Store/store'
 const Options = () => {
+
   return (
     <div className='w-screen h-screen flex flex-col'>
       <Link className='w-auto text-center rounded-lg m-2 h-8 border' to="/Geoguesser">Geoguesser</Link>
       <Link className='w-auto text-center rounded-lg m-2 h-8 border' to="/Nearme">Nearme</Link>
+      <Map/>
     </div>
   )
 }
