23b78a364714254f7d1c22691f4bc26f0caac4c9
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-03-30 16:33:36 +0530

    login-

--- Frontend/src/Components/Login-Register/Login.jsx
+++ Frontend/src/Components/Login-Register/Login.jsx
@@ -1,4 +1,37 @@
+import { GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAa08mNoFwQ66uDPyuvJBgVTI6I5Cgyr54",
+  authDomain: "mapplshackathon.firebaseapp.com",
+  projectId: "mapplshackathon",
+  storageBucket: "mapplshackathon.appspot.com",
+  messagingSenderId: "1045681932336",
+  appId: "1:1045681932336:web:070657638a7429123b85f5"
+};
+
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+import { getAuth, signInWithPopup } from "firebase/auth";
+import { Link} from 'react-router-dom';
+
 const Login=()=>{
+  
+const auth = getAuth();
+const signInWithGoogle=()=>{
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+ 
+  });
+}
+
   return(
     <>
     <div className="w-screen h-screen flex items-center">
@@ -6,8 +39,8 @@ const Login=()=>{
             <input id="E-mail" type="text" placeholder="Enter Your Email" className="border w-48 h-10 rounded-lg m-4"/>
             <input id="Password" type="text" placeholder="Enter Your Password" className="border w-48 h-10 rounded-lg m-4"/>
             <div className="buttons flex min-w-max">
-                <a href="/register" className="SignIn border rounded-lg bg-slate-100 text-black m-4 w-16 h-8 text-center">Sign Up</a>
-                <a href="/options" className="LogIn border rounded-lg bg-slate-100 text-black m-4 w-16 h-8 text-center">Log In</a>
+                <Link to="/register" className="SignIn border rounded-lg bg-slate-100 text-black m-4 w-16 h-8 text-center">Sign Up</Link>
+                <Link to="/options" className="LogIn border rounded-lg bg-slate-100 text-black m-4 w-16 h-8 text-center">Log In</Link>
             </div>
         </div>
     </div>
