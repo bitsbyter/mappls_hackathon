c36927c52504c5740b6e725472fb23e6dd3f3d97
OnkarDeshmukh67 <120507028+OnkarDeshmukh67@users.noreply.github.com>
2024-03-31 01:32:20 +0530

    finished basic carousel and made some changes to radius

--- Frontend/src/Components/Carousel/Carousel.jsx
+++ Frontend/src/Components/Carousel/Carousel.jsx
@@ -1,49 +1,61 @@
-import React , {useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import { GrCaretPrevious } from "react-icons/gr";
+import { GrCaretNext } from "react-icons/gr";
+
 import axios from 'axios'
 
-const Carousel = () => {
+const Carousel = ({ interval = 1000 }) => {
 
-  const [data, setData] = useState(
+  const [data , setData] = useState(
     [
       {
         id : 0,
-        text : "interesting"
-      } , 
+        text : 'hello'
+      },
       {
         id : 1,
-        text : "omoshirei dana"
+        text : 'there'
       },
       {
         id : 2,
-        text : 'sugoi jane'
+        text : 'welcome'
       }
     ]
-  );
+  )
 
-  // useEffect(() => {
-  //   axios.get('https://api.example.com/data')
-  //     .then(response => {
-  //       setData(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching data: ', error);
-  //     });
-  // }, []);
+  const [index , setIndex] = useState(0)
+
+  // useEffect(()=>{
+  //   axios.get('api')
+  //   .then(response => {
+  //     setData(response.data)
+  //   })
+  // } , [])
+
+  const next = () => {
+    if (index == data.length-1) {
+      setIndex(0)
+    } else setIndex(index + 1)
+  }
+  const previous = () => {
+    if (index == 0) {
+      setIndex(data.length - 1)
+    } else setIndex(index-1)
+  }
 
   return (
-    <div className='carousel_wrapper w-screen h-screen flex justify-center item-center overflow-hidden'>
-      {data.map(el => {
-        return(
-          <>
-            <div className='min-w-48 h-32 flex flex-col border justify-center items-center m-6'>
-              <p className='m-4'>{el.id}</p>
-              <p className='m-4'>{el.text}</p>
-            </div>        
-          </>
-        )
-      })}
+    <div className="w-screen h-screen overflow-hidden flex flex-col items-center justify-center">
+      <div className='flex justify-center items-center w-auto h-auto'>
+        <GrCaretPrevious onClick={previous} className='scale-150 m-2'/>
+        <div className='border w-60 h-96 justify-center items-center m-2 rounded-xl flex flex-col'>
+
+          <p> {data[index].id} </p> 
+          <p> {data[index].text} </p>
+        </div>
+        <GrCaretNext onClick={next} className='scale-150 m-2'/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
