eff8850347a5312f7a9f4abb376e9b8d189df88e
OnkarDeshmukh67 <120507028+OnkarDeshmukh67@users.noreply.github.com>
2024-04-01 02:36:31 +0530

    made design specific pages

--- Frontend/src/Components/Options/Geoguesser/Geoguesser.jsx
+++ Frontend/src/Components/Options/Geoguesser/Geoguesser.jsx
@@ -1,6 +1,10 @@
 import React, { useState , useEffect } from 'react'
 import CountdownTimer from '../../Timer-Image/Timer.jsx'
 import QuestionImage from './QuestionImage.jsx'
+import Map from '../../Map.jsx'
+
+import BackgroundImage from '../../../assets/Images/background.png'
+import logo from '../../../assets/Images/logo.png'
 
 const Geoguesser = () => {
 
@@ -10,14 +14,49 @@ const Geoguesser = () => {
   }
   return (
     <>
-    <div className='w-screen h-screen flex items-center flex-col'>
+    <div className='background bg-[#FBBC05] w-screen h-screen bg-cover bg-center fixed -z-50'>
+      <img className='w-full h-full' src={BackgroundImage} alt="" />
+    </div>
 
-      <div className='border w-full h-24 flex justify-center items-center'>   
-        <CountdownTimer minutes={3} seconds={0} isRunning={isRunning}/>
-        {!isRunning ? <button onClick={startGame} className='border rounded-lg bg-slate-100 text-black m-4 w-16 h-8 text-center'>Start</button> : <button onClick={startGame} className='border rounded-lg bg-slate-100 text-black m-4 w-16 h-8 text-center'>Stop</button>}
+    <div className='w-screen h-screen flex justify-between items-center bg-transparent/40'>
+
+      <div className='map-box w-4/5 h-5/6 rounded-2xl m-2 bg-black flex justify-between relative'>
+
+        <QuestionImage/>
+
+        <div className='w-auto h-full hidden'>
+          <Map />
+        </div>
+
+      </div>
+
+      <div className='timer-box w-1/5 h-5/6 flex flex-col rounded-2xl justify-center items-center bg-black'> 
+
+        <div className='w-full h-1/3 flex flex-col justify-center items-center'>
+
+          <img className='w-32 h-6 m-2' src={logo} alt="" />
+          <CountdownTimer minutes={3} seconds={0} isRunning={isRunning}/>
+          {!isRunning ? <button onClick={startGame} className='border rounded-lg bg-slate-100 text-black m-4 w-16 h-8 text-center'>Start</button> : <button onClick={startGame} className='border rounded-lg bg-slate-100 text-black m-4 w-16 h-8 text-center'>Stop</button>}
+
+        </div>
+
+        <div className='w-full h-1/2 flex flex-col'>
+          <div className='w-full h-16 flex flex-col p-4'>
+            <span className='text-[#FBBC05] text-lg'>Map :</span>
+            <p className='text-lg'>Cities of India</p>
+          </div>
+          <div className='w-full h-16 flex flex-col p-4'>
+            <span className='text-[#FBBC05] text-lg'>Round :</span>
+            <p className='text-lg'>Level 1/10</p>
+          </div>
+        </div>
+
+        <div className='w-full h-1/3 flex items-end justify-center'>
+          <button className='border w-32 h-12 rounded-3xl mb-4 bg-[#FBBC05] text-black text-xl'>Submit</button>
+        </div>
+
       </div>
       
-      <QuestionImage/>
     </div>
     </>
   )
--- Frontend/src/Components/Options/Options.jsx
+++ Frontend/src/Components/Options/Options.jsx
@@ -2,14 +2,39 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Map from '../Map'
 
-const Options = () => {
+import BackgroundImage from '../../assets/Images/background.png'
+import logo from '../../assets/Images/logo.png'
 
+import geoguess from '../../assets/Images/geo-guess.png'
+import locator from '../../assets/Images/locator.png'
+
+const Options = () => {
   return (
-    <div className='w-screen h-screen flex flex-col'>
-      <Link className='w-auto text-center rounded-lg m-2 h-8 border' to="/Geoguesser">Geoguesser</Link>
-      <Link className='w-auto text-center rounded-lg m-2 h-8 border' to="/Nearme">Nearme</Link>
-      <Map/>
+    <>  
+    <div className='background bg-[#FBBC05] w-screen h-screen bg-cover bg-center fixed -z-50'>
+      <img className='w-full h-full' src={BackgroundImage} alt="" />
     </div>
+
+    <div className='w-screen h-screen flex flex-col bg-transparent/45'>
+
+      <div className='logo-container flex flex-col justify-center items-center w-full h-1/4'>
+        <img className='w-96 m-2' src={logo} alt="" />
+        <p className='italic m-2 text-lg'>{`"Explore the world one click at a time."`}</p>
+      </div>
+
+      <div className='mt-1/2 w-full h-3/4 flex justify-center items-center'> 
+         <span className='options-button flex justify-center items-center rounded-md m-2 w-44 h-12 bg-black'>
+          <img className='h-6 w-6 m-2' src={geoguess} alt="" />
+          <Link className='m-2' to={'/geoguesser'}>Geoguesser</Link>
+         </span>
+         <span className='options-button flex justify-center items-center rounded-md m-2 w-44 h-12 bg-black'>
+          <img className='h-6 w-6 m-2' src={locator} alt="" />
+          <Link className='m-2' to={'/nearme'}>Near-me</Link>
+         </span>
+      </div>
+      
+    </div>
+    </>
   )
 }
 
