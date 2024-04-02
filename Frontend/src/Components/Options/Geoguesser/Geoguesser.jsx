20bc72de626cdecc5e3acfaa847c8fc48992baf4
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-04-03 02:02:47 +0530

    made changes

--- Frontend/src/Components/Options/Geoguesser/Geoguesser.jsx
+++ Frontend/src/Components/Options/Geoguesser/Geoguesser.jsx
@@ -26,7 +26,7 @@ function Answermap() {
 
 const Geoguesser = () => {
   const navigate=useNavigate();
-  const [isRunning, setisRunning] = useState(false);
+  const [isRunning, setisRunning] = useState(true);
   const [showMap, setShowMap] = useState(false);
   const [showScoreCard, setShowScoreCard] = useState(false);
 
@@ -57,11 +57,11 @@ const Geoguesser = () => {
 
       <div className='map-box w-4/5 h-5/6 rounded-2xl m-2 bg-black flex justify-center relative'>
         {showMap ? <Answermap /> : null}
-        {!isRunning ?   <StreetViewMap /> : null}
+        {isRunning ?   <StreetViewMap /> : null}
         {/* <StreetViewMap /> */}
         {/* score card ko bhi location select karne ke baad hi render karna h */}
         {/* <ScoreCard /> */}
-        {/* {showScoreCard ? <ScoreCard /> : null} */}
+        {showScoreCard ? <ScoreCard /> : null}
       </div>
 
           <div className='timer-box w-1/5 h-5/6 flex flex-col rounded-2xl justify-center items-center bg-black'>
@@ -92,7 +92,7 @@ const Geoguesser = () => {
           </div>
         </div>
       </div>
-      {showScoreCard && <ScoreCard />}
+      
     </>
   );
 }
