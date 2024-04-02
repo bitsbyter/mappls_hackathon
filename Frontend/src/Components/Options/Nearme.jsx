f994f6305cb757d5fc70e5a559fa8659a1575371
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-04-03 03:19:40 +0530

    made changes to backend

--- Frontend/src/Components/Options/Nearme.jsx
+++ Frontend/src/Components/Options/Nearme.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Radius from '../Carousel/Radius'
+import Carousel from '../Carousel/Carousel'
 import Loader from '../Loader'
 import { useDispatch, useSelector } from 'react-redux'
 import axios from "axios"
@@ -7,9 +8,8 @@ import {Link} from 'react-router-dom'
 import { CarouselDataActions } from '../../Store/CarouselDataSlice'
 import BackgroundImage from '../../assets/Images/background.png'
 import logo from '../../assets/Images/logo.png'
+import { useEffect } from 'react'
 import home from '../../assets/Images/home.png'
-
-
 function LocationType() {
   const [locationType , setLocationType] = useState('');
 
@@ -30,17 +30,18 @@ function LocationType() {
   );
 }
 
+
 const Nearme = () => {
   const dispatch=useDispatch();
   const userCoords = useSelector((store) => store.userCoords);
     const radius=useSelector((store)=>store.userRadius)
     
         const getCarouselData = () => {
-            axios.get("backend-kohl-chi.vercel.app",{
+            axios.get("http://localhost:3000",{
                 params:{
                     lat:userCoords.lat,
                     lng:userCoords.long,
-                    radius:radius
+                    radius:5000
                 }
             })
             .then((response)=>{
@@ -58,7 +59,8 @@ const Nearme = () => {
     getCarouselData();
     !isCarousel ? setIsCarousel(true) : setIsCarousel(false)
   }
-
+  
+  //Onkar make an navbar typeof thing for user to search for the category of things they want to search
   return (
     
     <> 
@@ -67,21 +69,20 @@ const Nearme = () => {
     </div>
 
     <Link to={'../options'} className='absolute top-0 left-0 w-8 h-8'><img src={home} alt="" /></Link>
-
     <div className='w-screen h-screen flex justify-center items-center'>
 
       <div className='select-radius h-auto w-auto flex'>
 
         <div className='w-screen h-screen flex justify-center items-center flex-col'>
 
-          <div className='w-2/3 h-2/5 lg:w-5/12 rounded-xl flex flex-col justify-between items-center bg-black/70'>
-            <img className='h-12 w-60 m-2' src={logo} alt="" />
-            <span className='max-w-60 text-sm monseratt text-center'>Explore the areas near you ~ Please specify the radius :</span>
+          <div className='w-2/3 h-2/5 rounded-xl flex flex-col justify-between items-center bg-black/70  '>
+            <img className='h-12 w-96 m-2' src={logo} alt="" />
+            <span className=' text-lg'>{`Explore the loactions near you`}</span>
+            <span className=' text-lg'>{`Please select your radius`}</span>
             <Radius />
 
             <LocationType />
-
-            <Link to={"/carousel"} onClick={loadCarousel} className='w-24 h-7 mb-2 bg-[#FBBC05] text-black rounded-md m-2 flex justify-center items-center monseratt font-bold'>Explore</Link>
+            <Link to="/carousel" onClick={loadCarousel} className='w-24 h-7 mb-2 bg-[#FBBC05] text-black rounded-md m-2 flex justify-center items-center'>Explore</Link>
           </div>
         
         </div>
