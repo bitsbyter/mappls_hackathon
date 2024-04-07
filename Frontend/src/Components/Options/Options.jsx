f2c935dcdaff0d70c0a8e60499c6db42bdde5ab8
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-04-07 07:41:09 +0530

    made some changes

--- Frontend/src/Components/Options/Options.jsx
+++ Frontend/src/Components/Options/Options.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { IoHandLeft, IoSearchSharp } from "react-icons/io5";
 import { Link } from 'react-router-dom'
 import { QuestionAction } from '../../Store/AskedPlaceSlice.js'
 import { useDispatch, useSelector } from 'react-redux'
@@ -10,6 +11,7 @@ import locator from '../../assets/Images/locator.png'
 import { useState,useEffect } from 'react'
 import axios from "axios"
 import { userCategoryActions } from '../../Store/CurrentUserPointsPrefSlice.js'
+
 
 const textSearch=async(location)=>{
       try{
@@ -69,21 +71,27 @@ const Options = () => {
     }
 }, [questionObject, dispatch]);
 
-
+  const handleSearchClick=()=>{
+    console.log("I was CLicked")
+  }
 
   return (
     <>  
     <div className='background bg-[#FBBC05] w-screen h-screen bg-cover bg-center fixed -z-50'>
       <img className='w-full h-full ' src={BackgroundImage} alt="" />
     </div>
-
+    <div className='absolute z-10 right-20 top-20'>
+    <IoSearchSharp  onClick={handleSearchClick}/>
+    
+      </div>
     <div className='w-screen h-screen flex flex-col bg-transparent/45'>
 
       <div className='logo-container flex flex-col justify-center items-center w-full h-1/4'>
         <img className='w-64 sm:w-96 m-4' src={logo} alt="" />
+        
         <p id='satisfy-regular' className='m-2 text-xl tracking-widest leading-8 text-[#FBBC05] monseratt'>{`"Explore the world one click at a time."`}</p>
       </div>
-
+      
       <div className='mt-1/2 w-full h-3/4 flex flex-col sm:flex-row justify-center items-center'> 
          <span className='options-button flex justify-center items-center rounded-md m-2 w-60 h-16 xl:w-72 xl:h-20 bg-black'>
           <img className='h-8 w-8 m-2 xl:h-12 xl:w-12' src={geoguess} alt="" />
